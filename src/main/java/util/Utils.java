51fa07d4cc6ddc4766f54a9db080125ea4ba7e81
sam <samuel.klein@online.de>
2018-02-13 01:34:31 +0100

    added method to decode key string identifier into integer pair

--- src/main/java/util/Utils.java
+++ src/main/java/util/Utils.java
@@ -5,7 +5,6 @@ import javafx.util.Pair;
 public class Utils {
 
     public static Pair<Integer, Integer> getClusterForCoordinates(int x, int y){
-
         int facX = x/100;
         if(x < 0){
             facX -= 1;
@@ -16,12 +15,18 @@ public class Utils {
             facY -= 1;
         }
         int startY = 100*facY;
-
         return new Pair<Integer, Integer>(startX, startY);
     }
 
     public static String getKeyString(int x, int y){
         return x+"_"+y;
+    }
+
+    public static Pair<Integer, Integer> decodeKeyString(String keyString){
+        String[] tmpArr = keyString.split("_");
+        int x = Integer.parseInt(tmpArr[0]);
+        int y = Integer.parseInt(tmpArr[1]);
+        return new Pair<Integer, Integer>(x, y);
     }
 
 }
